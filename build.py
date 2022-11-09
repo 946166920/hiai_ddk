610cc5e012c7c434b762a070bf50ae77767d679b
杨善军 <hiai_ddk@qq.com>
2022-11-09 09:52:15 +0800

    Match-id-525f4415c5652ae007c7f1dee5fd399e3040f846

--- build.py
+++ build.py
@@ -20,11 +20,10 @@ THIRD_PARTY_LINK_LIST = [
     "http://10.136.104.231:4543/libboundscheck-1.1.11.zip",
     "http://10.136.104.231:4543/protobuf-3.13.0.zip",
     "http://10.136.104.231:4543/protoc-3.13.0-linux-x86_64.zip",
-    "http://10.136.104.231:4543/cmake-3.21.4-linux-x86_64.tar.gz",
     "http://10.136.104.231:4543/android-ndk-r20b-linux-x86_64.zip",
 
     # "http://10.136.104.231:4543/android-ndk-r23b-linux.zip",
-    # "http://10.136.104.231:4543/cmake-3.20.5-linux-x86_64.tar.gz"
+    "http://10.136.104.231:4543/cmake-3.20.5-linux-x86_64.tar.gz"
 ]
 
 THIRD_PARTY_LINK_LIST_URL = [
@@ -73,8 +72,6 @@ def decopress_tools():
     # os.system(chmod_ndk)
 
     # cmake 加export
-    # export_cmake_path = "export PATH=third_party/cmake-3.21.4-linux-x86_64/bin:$PATH"
-    # os.system(export_cmake_path)
     export_cmake_path = "export PATH=third_party/cmake-3.20.5-linux-x86_64/bin:$PATH"
     os.system(export_cmake_path)
     # c_src 换目录名
